bc19134aefa1f6e7139aed869dc7018852be38a8
Matias Martin <matias.martinivan@gmail.com>
2020-07-10 21:42:38 -0300

    Funcionando por consola

--- InteresesBancarios/InteresesBancarios/Intereses.cs
+++ InteresesBancarios/InteresesBancarios/Intereses.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace InteresesBancarios
 {
-    class Intereses
+    class Intereses : Visitor
     {
+        private List<float> intereses;
+
+        public double visit(CajaDeAhorro cajaAhorro)
+        {
+            return 0.01 * cajaAhorro.getMonto() + cajaAhorro.getMonto();
+        }
+        public double visit(CuentaCorriente cuentaCorriente)
+        {
+            return 0.01 * cuentaCorriente.getMonto() + cuentaCorriente.getMonto();
+        }
+        public double visit(TarjetaDeCredito tarjeta)
+        {
+            return 0.05 * tarjeta.getMonto() + tarjeta.getMonto();
+        }
     }
 }
--- InteresesBancarios/InteresesBancarios/Program.cs
+++ InteresesBancarios/InteresesBancarios/Program.cs
@@ -6,7 +6,30 @@ namespace InteresesBancarios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TarjetaDeCredito tarjeta = new TarjetaDeCredito();
+            tarjeta.setMonto(12000);
+
+            CajaDeAhorro caja = new CajaDeAhorro();
+            caja.setMonto(80000);
+
+            CuentaCorriente cuentacorriente = new CuentaCorriente();
+            cuentacorriente.setMonto(30000);
+
+            Intereses intereses = new Intereses();
+
+            Console.WriteLine("Tarjeta sin intereses");
+            Console.WriteLine(tarjeta.getMonto());
+            Console.WriteLine("Caja de ahorro sin intereses");
+            Console.WriteLine(caja.getMonto());
+            Console.WriteLine("Cuenta corriente sin intereses");
+            Console.WriteLine(cuentacorriente.getMonto());
+
+            Console.WriteLine("Tarjeta con intereses");
+            Console.WriteLine(tarjeta.accept(intereses));
+            Console.WriteLine("Caja de ahorro con intereses");
+            Console.WriteLine(caja.accept(intereses));
+            Console.WriteLine("Cuenta corriente con intereses");
+            Console.WriteLine(cuentacorriente.accept(intereses));
         }
     }
 }
